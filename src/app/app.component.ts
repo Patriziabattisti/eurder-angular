08895251a89c11fc542b37c600e362c0340f1a29
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-05-04 17:22:45 +0200

    trying to change the nav bar color while clicking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Eurder';
+
+  changeColor() {
+
+    let pathName = location.href;
+    let index = pathName.lastIndexOf('/') + 1;
+    let nameOfNav = pathName.substr(index);
+    console.log(nameOfNav);
+    let monA = document.getElementById(nameOfNav);
+    monA.className = 'active';
+
+    console.log(monA.className);
+  }
 }
