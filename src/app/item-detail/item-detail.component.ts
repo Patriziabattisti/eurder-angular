ca2adc69e97ce3b19e5048d24305a2de1470c65e
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-05-06 10:48:31 +0200

    changed the update html into a form

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input} from '@angular/core';
 import {Item} from '../item';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
+import {FormBuilder, FormControl} from '@angular/forms';
 
 import {ItemService} from '../item.service';
 
@@ -13,18 +14,25 @@ import {ItemService} from '../item.service';
 export class ItemDetailComponent implements OnInit {
   @Input() item: Item;
 
-  constructor(private route: ActivatedRoute, private itemService: ItemService, private location: Location) {
+  constructor(private route: ActivatedRoute, private itemService: ItemService, private location: Location, private formBuilder: FormBuilder) {
   }
-
   ngOnInit(): void {
     this.getItem();
+
   }
 
   getItem(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.itemService.getItem(id).subscribe(item => this.item = item);
   }
-  goBack(): void {
-    this.location.back();
+
+  onSubmit(itemData) {
+    console.log(itemData);
+    itemData.name = itemData.name.trim();
+
+    if (!itemData.name || itemData.price < 0 || itemData.amountOfStock < 0) {
+      return;
+    }
+    // need to implement the update method
   }
 }
